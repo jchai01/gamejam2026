793dd128451f7cf593215efc6f815706a112f136
Joseph <josephchai100@hotmail.com>
2026-04-17 02:55:37 +0100

    scaffold till mission 3 + fix flow

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { IntroScene } from "./scenes/intro-scene.js";
 import { MenuScene } from "./scenes/menu-scene.js";
-import { M1Scene } from "./scenes/m1.js";
-import { M1ReturnScene } from "./scenes/m1-return.js";
-import { M2Scene } from "./scenes/m2.js";
 import { CheckpointScene } from "./scenes/checkpoint-scene.js";
+import { M1Scene } from "./scenes/m1.js";
+import { M2Scene } from "./scenes/m2.js";
+import { M3Scene } from "./scenes/m3.js";
+import { M1ReturnScene } from "./scenes/m1-return.js";
+import { M2ReturnScene } from "./scenes/m2-return.js";
+import { M3ReturnScene } from "./scenes/m3-return.js";
 
 const config = {
   type: Phaser.AUTO,
@@ -21,6 +24,9 @@ const config = {
     M1Scene, // M1 - Mission 1
     M1ReturnScene,
     M2Scene,
+    M2ReturnScene,
+    M3Scene,
+    M3ReturnScene,
     CheckpointScene,
   ],
   scale: {

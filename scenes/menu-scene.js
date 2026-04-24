953ac878b1d5db41acc3ba02a6facd0e6ed3a642
Joseph <josephchai100@hotmail.com>
2026-04-25 00:26:20 +0100

    menu UI change

--- scenes/menu-scene.js
+++ scenes/menu-scene.js
@@ -11,7 +11,6 @@ export class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    // Initialize the global game state here
     if (!this.registry.has("stage")) {
       this.registry.set("stage", 1);
     }
@@ -20,31 +19,43 @@ export class MenuScene extends Phaser.Scene {
     if (this.registry.get("stage") == 1) {
       this.add.image(0, 0, 'menuBg1').setOrigin(0);
       this.add
-        .text(this.scale.width / 2, 100, "Mission 1", { fontSize: "38px" })
-        .setOrigin(0.5);
+        .text(this.scale.width / 2, 100, "Mission 1", {
+          fontSize: "38px", fontStyle: "Bold",
+        })
+        .setOrigin(0.5).setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
     }
     else if (this.registry.get("stage") == 2) {
       this.add.image(0, 0, 'menuBg2').setOrigin(0);
       this.add
-        .text(this.scale.width / 2, 100, "Mission 2", { fontSize: "38px" })
-        .setOrigin(0.5);
+        .text(this.scale.width / 2, 100, "Mission 2", {
+          fontSize: "38px", fontStyle: "Bold",
+        })
+        .setOrigin(0.5).setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
     }
     else if (this.registry.get("stage") == 3) {
       this.add.image(0, 0, 'menuBg3').setOrigin(0);
       this.add
-        .text(this.scale.width / 2, 100, "Mission 3", { fontSize: "38px" })
-        .setOrigin(0.5);
+        .text(this.scale.width / 2, 100, "Mission 3", {
+          fontSize: "38px", fontStyle: "Bold",
+        })
+        .setOrigin(0.5).setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
     } else {
+      this.add
+        .text(this.scale.width / 2, 100, "Mission 4", {
+          fontSize: "38px", fontStyle: "Bold",
+        })
+        .setOrigin(0.5).setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
       this.add.image(0, 0, 'menuBg4').setOrigin(0);
 
     }
-
     this.registry.set("shipWidth", 0.035);
 
-
     this.add
-      .text(this.scale.width / 2, 800, "SPACE to take off", { fontSize: "38px" })
-      .setOrigin(0.5);
+      .text(this.scale.width / 2, 180, "SPACE to take off", {
+        fontSize: "38px",
+        fontStyle: "Bold",
+      })
+      .setOrigin(0.5).setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
 
     this.input.keyboard.once("keydown-SPACE", () => {
       if (this.registry.get("stage") === 1) {
@@ -53,8 +64,6 @@ export class MenuScene extends Phaser.Scene {
         this.scene.start("M2Scene");
       }
     });
-
-
 
     // FOR DEBUGGING
     this.input.keyboard.on("keydown-TWO", () => {
